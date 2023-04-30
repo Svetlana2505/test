91bd7a12c30162dce9a73c1a03069a6cb477272d
Svetlana2505 <svetikred77@gmail.com>
2023-04-30 17:11:56 +0000

    Deploying to gh-pages from @ Svetlana2505/test@689ea9815a5b6cc216a510fa0a931517ad7bc452 🚀

---
+++ index.7d51b075.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll(".questions-item");e.forEach((function(t,i){e[i].addEventListener("click",(function(){t.classList.toggle("active");var e=t.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"}))}))}();
+//# sourceMappingURL=index.7d51b075.js.map
---
+++ index.e14bac34.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".questions-item");e.forEach(((t,l)=>{e[l].addEventListener("click",(()=>{t.classList.toggle("active");const e=t.nextElementSibling;e.style.maxHeight?e.style.maxHeight=null:e.style.maxHeight=e.scrollHeight+"px"}))}));
+//# sourceMappingURL=index.e14bac34.js.map
